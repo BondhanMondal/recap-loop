89f25de2cd624a20e61ddfedc344e47b9b88da40
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-29 20:48:14 +0600

    for loop added

---
+++ for-loop.js
@@ -0,0 +1,22 @@
+
+// console.log('running a for loop');
+
+// for (var roastGiven = 0; roastGiven <7; roastGiven++){
+//     console.log('roast den, Please !!');
+//     console.log(roastGiven);
+// }
+
+// for (var i= 1; i < 11; i++) {
+//     console.log(i);
+// }
+
+//odd
+// for (var i= 1; i < 21; i += 2) {
+//     console.log(i);
+// }
+
+//even numbers
+for (var i= 0; i < 21; i += 2) {
+    console.log(i);
+}
+
